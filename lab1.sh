4800547f0560dd51db55b733a005463fa00c642a
acavataio <anthony.p.cavataio@gmail.com>
2017-04-04 15:10:32 -0700

    Update lab1.sh

--- lab1.sh
+++ lab1.sh
@@ -5,3 +5,7 @@ whoami #Stands for who am I
 ls #lists files in directory, use -l flag
 cd #stands for call directory, go to other directories
 date #what time is it
+
+!#/bin/sh                                                                       
+                                                                                
+echo "testing 123"
