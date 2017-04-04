65ce2b538051f081eda11eb09d28d8e9dc666390
acavataio <anthony.p.cavataio@gmail.com>
2017-04-04 14:47:24 -0700

    Update lab1.sh

--- lab1.sh
+++ lab1.sh
@@ -1,1 +1,7 @@
 #this is a comment
+
+pwd #Stands for Print working directory  (where)
+whoami #Stands for who am I
+ls #lists files in directory, use -l flag
+cd #stands for call directory, go to other directories
+date #what time is it
